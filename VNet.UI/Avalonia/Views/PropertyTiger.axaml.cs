b468ff643700d82a32ae1a6519a4c5e15eb3e742
PrimeEagle <misc@hiddendragon.com>
2023-10-19 02:08:23 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.UI@1407 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

---
+++ VNet.UI/Avalonia/Views/PropertyTiger.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace VNet.UI.Avalonia.Views
+{
+    public partial class PropertyTiger : UserControl
+    {
+        public PropertyTiger()
+        {
+            InitializeComponent();
+        }
+    }
+}
