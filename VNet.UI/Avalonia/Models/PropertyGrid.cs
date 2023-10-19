ffad73f3a37056960c06d2ddeabf9ce7fb4154e9
PrimeEagle <misc@hiddendragon.com>
2023-10-19 02:14:00 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.UI@1408 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

--- VNet.UI/Avalonia/Models/PropertyGrid.cs
+++ VNet.UI/Avalonia/Models/PropertyGrid.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VNet.UI.Avalonia.Models
+﻿namespace VNet.UI.Avalonia.Models
 {
-    internal class PropertyGrid
+    public class PropertyGrid
     {
     }
 }
