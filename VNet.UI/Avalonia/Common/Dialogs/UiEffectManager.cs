07d73eed3152d255f096c1edad1fc2c944866836
PrimeEagle <misc@hiddendragon.com>
2023-10-24 17:33:17 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.UI@1433 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

--- VNet.UI/Avalonia/Common/Dialogs/UiEffectManager.cs
+++ VNet.UI/Avalonia/Common/Dialogs/UiEffectManager.cs
@@ -1,9 +1,13 @@
-﻿using Avalonia;
-using Avalonia.Animation;
-using Avalonia.Animation.Easings;
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
 using Avalonia.Media;
-using Avalonia.Styling;
+
+// ReSharper disable SuggestBaseTypeForParameter
+// ReSharper disable MemberCanBeMadeStatic.Local
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning disable CS8601 // Possible null reference assignment.
+#pragma warning disable CA1822
 
 namespace VNet.UI.Avalonia.Common.Dialogs
 {
@@ -12,7 +16,6 @@ namespace VNet.UI.Avalonia.Common.Dialogs
         private readonly Window _parentWindow;
         private readonly Control _parentContent;
         private readonly object _originalContent;
-        private readonly IEffect _originalEffect;
         private Border _overlay;
 
         public UiEffectManager(Window parentWindow)
@@ -20,171 +23,110 @@ namespace VNet.UI.Avalonia.Common.Dialogs
             _parentWindow = parentWindow;
             _parentContent = (Control)parentWindow.Content;
             _originalContent = parentWindow.Content;
-            _originalEffect = _parentContent.Effect;
         }
 
-        public void ApplyEffects()
+        public async void ApplyEffects()
         {
-            //ApplyBlur();
-            ApplyDarken();
-        }
-
-        private void ApplyBlur()
-        {
-            _parentContent.Effect = new BlurEffect { Radius = 5 };
-        }
-
-        private async void ApplyDarken()
-        {
-            //DoubleTransition gridTransition = new DoubleTransition();
-            //gridTransition.Duration = TimeSpan.FromSeconds(10);
-            //gridTransition.Property = global::Avalonia.Visual.OpacityProperty;
-
-            // For mainPanel fades
-            //DoubleTransition borderTransition = new DoubleTransition();
-            //borderTransition.Duration = TimeSpan.FromSeconds(10);
-            //borderTransition.Property = global::Avalonia.Visual.OpacityProperty;
-
-            // Create a new style
-            var overlayStyle = new Style(x => x.OfType<Border>());
-
-            // Define the transition
-            var transitions = new Transitions();
-            transitions.Add(new DoubleTransition
-            {
-                Property = Border.OpacityProperty,
-                Duration = TimeSpan.FromSeconds(5), // or whatever duration you prefer
-            });
-
+            // Create an overlay with zero opacity initially
             _overlay = new Border
             {
-                Background = new SolidColorBrush(Colors.Black, 0),
+                Background = new SolidColorBrush(Colors.Black),
                 IsHitTestVisible = true,
                 Opacity = 0
             };
 
-            _overlay.Styles.Add(overlayStyle);
+            // Assuming _parentContent is a container control (like Panel, Grid, etc.)
+            // that can have multiple children, you add the overlay to it.
+            if (_parentContent is Panel parentPanel)
+            {
+                parentPanel.Children.Add(_overlay);
+            }
 
-            // Set the "Transitions" property through the style
-            overlayStyle.Setters.Add(new Setter(Border.TransitionsProperty, transitions));
-
-            // Optionally, you might want to set an initial Opacity as well
-            overlayStyle.Setters.Add(new Setter(Border.OpacityProperty, 0)); // Initial opacity
-
-            //var transition = new DoubleTransition()
-            //{
-            //    Property = Border.OpacityProperty,
-            //    Duration = TimeSpan.FromMilliseconds(2000),
-            //    Easing = new LinearEasing()
-            //};
-
-            // Add to the visual tree
-            var grid = new Grid();
-            grid.Children.Add(_overlay);
-            _parentWindow.Content = grid;
-
-            // Trigger the transition after the element is rendered, possibly with a slight delay
-            await Task.Delay(100); // This delay is arbitrary; you may need to adjust this.
-            _overlay.Opacity = 0.7; // Or whatever your target opacity value is.
-
-
-            //_overlay = new Border
-            //{
-            //    Background = new SolidColorBrush(Colors.Black, 0),
-            //    IsHitTestVisible = true,
-            //    Opacity = 0
-            //};
-            //_overlay.Transitions = new Transitions();
-            //_overlay.Transitions.Add(borderTransition);
-
-            //var grid = new Grid();
-            //grid.Transitions = new Transitions();
-
-            //grid.Children.Add(_overlay);
-            //_parentWindow.Content = grid;
-
-            //await Task.Delay(1000);
-            //_parentWindow.UpdateLayout();
-            //_overlay.Opacity = 0.2;
-            //await FadeIn(_overlay);
+            // Now, fade in the overlay for the darkening effect.
+            // You don't have to set the _parentWindow.Content again since you're modifying its current content.
+            await FadeIn(_overlay, _parentContent);
         }
 
-        //private async void ApplyDarken()
-        //{
-        //    // Create an animation instance
-        //    var animation = new Animation
-        //    {
-        //        Duration = TimeSpan.FromSeconds(10), // Define the same duration
-        //        FillMode = FillMode.Both, // Maintain the animation's state after it finishes
-        //        IterationCount = new IterationCount(1), // Run the animation once
-        //    };
-
-        //    // Define what property the animation applies to and the keyframes
-        //    animation.Children.Add(new KeyFrame
-        //    {
-        //        Setters =
-        //        {
-        //            new Setter
-        //            {
-        //                Property = Visual.OpacityProperty,
-        //                Value = 0.2d, // The target opacity value
-        //            },
-        //        },
-        //        Cue = new Cue(1f) // The time cue (1f represents the end of the animation timeline)
-        //    });
-
-        //    _overlay = new Border
-        //    {
-        //        Background = new SolidColorBrush(Colors.Black),
-        //        IsHitTestVisible = true,
-        //        Opacity = 0 // Set initial opacity to 0
-        //    };
-
-        //    var grid = new Grid();
-        //    grid.Children.Add(_overlay);
-
-        //    _parentWindow.Content = grid;
-
-        //    // Now, apply and start the animation
-        //    await animation.RunAsync(_overlay);
-        //}
-
-        private async Task FadeIn(Border border)
+        private async Task FadeIn(Border overlay, Control originalContent)
         {
             const int animationSteps = 3;
-            const double targetOpacity = 0.2;
-            const int animationDuration = 150; // in milliseconds
+            const double targetOpacity = 0.2; // Final opacity for the overlay
+            const double targetBlurRadius = 5; // Final blur radius for the content
+            const int animationDuration = 150; // Total duration for the animation
 
-            for (int step = 1; step <= animationSteps; step++)
+            // Set the initial state of the overlay and blur effect
+            overlay.Background = new SolidColorBrush(Colors.Black);
+            var blur = new BlurEffect { Radius = 0 };
+            originalContent.Effect = blur; // Applying the blur to the original content
+
+            for (var step = 1; step <= animationSteps; step++)
             {
-                double opacity = step * targetOpacity / animationSteps;
-                border.Background = new SolidColorBrush(Colors.Black, opacity);
+                var opacity = step * targetOpacity / animationSteps;
+                var blurRadius = step * targetBlurRadius / animationSteps;
 
+                // Adjusting the opacity of the overlay, not the content itself
+                overlay.Opacity = opacity;
+
+                // Increasing the blur effect on the original content
+                blur.Radius = blurRadius;
+
+                // Request the interface to redraw the updated visuals
+                originalContent.InvalidateVisual();
+
+                // Pause the execution for the step duration
                 await Task.Delay(animationDuration / animationSteps);
             }
         }
 
         public async Task RevertEffects()
         {
-            //await FadeOut(_overlay);
-            _overlay.Opacity = 0;
-            _parentContent.Effect = _originalEffect;
-            _parentWindow.Content = _originalContent;
+            // First, animate the fading out of the overlay and the removal of the blur effect.
+            await FadeOut(_overlay, _parentContent);  // This assumes FadeOut handles the animation logic correctly.
+
+            // After the animation, restore everything to its original state.
+
+            // Clear any remaining effects from the parent content.
+            _parentContent.Effect = null;
+
+            // Instead of setting the content again, we remove the overlay from the parent container.
+            // This ensures the original content remains intact and undisturbed.
+            if (_parentContent is Panel parentPanel && _overlay != null)
+            {
+                parentPanel.Children.Remove(_overlay);
+            }
+
+            // No need to reset _parentWindow.Content as we haven't replaced it, we've only added/removed children.
         }
 
-        private async Task FadeOut(Border border)
+        private async Task FadeOut(Border border, Control originalContent)
         {
             const int animationSteps = 3;
-            const double targetOpacity = 0;
+            const double targetOpacity = 0.2;
+            const double targetBlurRadius = 5;
             const int animationDuration = 150; // in milliseconds
 
-            for (int step = 1; step <= animationSteps; step++)
+            for (var step = 1; step <= animationSteps; step++)
             {
-                double opacity = 1 - (step * (1 - targetOpacity) / animationSteps);
-                border.Background = new SolidColorBrush(Colors.Black, opacity);
+                var opacity = targetOpacity - step * targetOpacity / animationSteps;
+                var blurRadius = targetBlurRadius - step * targetBlurRadius / animationSteps;
+
+                border.Opacity = opacity;
+
+                // Safely check if the Effect is a BlurEffect and if so, change its properties.
+                if (originalContent?.Effect is BlurEffect blur)
+                {
+                    blur.Radius = blurRadius;
+                }
+
+                originalContent?.InvalidateVisual();
 
                 await Task.Delay(animationDuration / animationSteps);
             }
+
+            // Assuming the effect should be removed afterward
+            originalContent.Effect = null;
+
+            // Additional cleanup if needed, for example, removing the overlay from its parent container
         }
     }
 }
---
+++ VNet.UI/WindowsSystemFontInfo.cs
@@ -0,0 +1,70 @@
+﻿namespace VNet.UI
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    public static class WindowsSystemFontInfo
+    {
+        [DllImport("gdi32.dll")]
+        private static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr GetStockObject(int fnObject);
+
+
+        // These values are from the Windows API documentation for the LOGFONT structure and DeviceCaps.
+        private const int LOGPIXELSY = 90;
+        private const int DEFAULT_GUI_FONT = 17;
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public struct LOGFONT
+        {
+            public int lfHeight;
+            public int lfWidth;
+            public int lfEscapement;
+            public int lfOrientation;
+            public int lfWeight;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string lfFaceName; // This specifies the name of the font face.
+        }
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto)]
+        public static extern bool GetObject(IntPtr hgdiobj, int cbBuffer, ref LOGFONT lpvObject);
+
+        public static (string fontName, double fontSize) GetSystemFontInfo()
+        {
+            IntPtr hDC = IntPtr.Zero;
+            try
+            {
+                hDC = GetDC(IntPtr.Zero);
+                int logpixelsy = GetDeviceCaps(hDC, LOGPIXELSY);
+
+                LOGFONT logFont = new LOGFONT();
+                IntPtr hFont = GetStockObject(DEFAULT_GUI_FONT);
+
+                if (GetObject(hFont, Marshal.SizeOf(logFont), ref logFont))
+                {
+                    // Convert the lfHeight, which is in logical units, to points
+                    double points = logFont.lfHeight * 72.0 / logpixelsy;
+
+                    return (logFont.lfFaceName, Math.Abs(points));  // Abs to ensure the value is non-negative
+                }
+
+                return (string.Empty, 0);  // You might want to handle this case differently
+            }
+            finally
+            {
+                if (hDC != IntPtr.Zero)
+                {
+                    ReleaseDC(IntPtr.Zero, hDC);
+                }
+            }
+        }
+    }
+}
